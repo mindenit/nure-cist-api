1e9ba2f6c429cdb11ebb98629216ea127f227d07
kkkiikkk <ruddnovskiy@gmail.com>
2023-09-06 20:31:19 +0300

    feat: add rate-limit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // Core
 import helmet from '@fastify/helmet';
 import cors from '@fastify/cors';
+import rateLimit from '@fastify/rate-limit';
 
 // Tools
 import { createApp } from './app';
@@ -9,39 +10,55 @@ import initDB from './db';
 import { scheduleRoute } from './routes/schedule';
 import { schedulingParserJob } from './jobs/scheduling';
 
-const app = createApp({
-  logger: config[env.NODE_ENV].logger,
-});
+(function () {
+  const app = createApp({
+    logger: config[env.NODE_ENV].logger,
+  });
 
-app.register(cors, {
-  origin: '*',
-  credentials: true,
-});
+  app.register(cors, {
+    origin: '*',
+    credentials: true,
+  });
 
-app.register(helmet);
+  app.register(helmet);
 
-
-app.get('/spec', async (_req, _res) => {
-  return app.swagger();
-});
-
-
-initDB().then().catch();
-
-app.register(scheduleRoute, { prefix: '/api' })
-schedulingParserJob().then().catch()
-
-
-app.listen(
-  {
-    port: env.PORT ?? 3000,
-    host: env.HOST ?? 'localhost'
-  },
-  (err, _address) => {
-    if (err) {
-      app.log.error(err);
-      process.exit(1);
+  app.register(rateLimit, {
+    ban: 2,
+    addHeadersOnExceeding: {
+      'x-ratelimit-limit': true,
+      'x-ratelimit-remaining': true,
+      'x-ratelimit-reset': true
+    },
+    addHeaders: {
+      'x-ratelimit-limit': true,
+      'x-ratelimit-remaining': true,
+      'x-ratelimit-reset': true,
+      'retry-after': true
     }
-  }
-);
+  })
+
+  app.get('/spec', async (_req, _res) => {
+    return app.swagger();
+  });
+
+
+  initDB().then().catch();
+
+  app.register(scheduleRoute, { prefix: '/api' })
+  schedulingParserJob().then().catch()
+
+
+  app.listen(
+      {
+        port: env.PORT ?? 3000,
+        host: env.HOST ?? 'localhost'
+      },
+      (err, _address) => {
+        if (err) {
+          app.log.error(err);
+          process.exit(1);
+        }
+      }
+  );
+})()
 
